32445d6ec9143e91e023ec7a6c40568544e2a893
Denis <omelched@gmail.com>
2021-04-17 01:50:49 +0300

    metrics

--- neuroAPI/neuralcore/__init__.py
+++ neuroAPI/neuralcore/__init__.py
@@ -1,1 +1,12 @@
+import torch
+from torch import nn
 
+from neuroAPI.neuralcore.models import NeuralNetwork
+model = NeuralNetwork(3)
+
+learning_rate = 1e-3
+batch_size = 64
+epochs = 1000
+
+loss_fn = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
