a1269d2a59c090fb1e0dbc4d0347ec0dfb43a390
Denis <omelched@gmail.com>
2021-09-28 20:22:13 +0300

    :bug: fix ImportError

--- gnmproj/apps/gnm/src/__init__.py
+++ gnmproj/apps/gnm/src/__init__.py
@@ -2,4 +2,4 @@ from .fields import UUIDAutoField
 from .cache import HashedDatabaseCache
 from .auth import JWTModelBackend
 from .utils import check_typing, requires_jwt
-from .exceptions import InvalidCredentials, generate_DNE
+from .exceptions import InvalidCredentials, generate_DNE, TaskDoesNotExist
